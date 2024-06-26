600e7e82e6a57685ada703e28de7f5fa7719c83b
stserk <stserkoniuk@exadel.com>
2024-06-26 18:40:16 +0300

    Basic UI

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -14,5 +15,29 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-
+def count_down(count):
+    window.after(1000, count_down, count - 1)
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+timer_laber = Label(text="Timer", font=(FONT_NAME, 35, "bold"), fg=GREEN, bg=YELLOW)
+timer_laber.grid(column=1, row=0)
+
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_marks = Label(text="🔴", fg=RED, bg=YELLOW)
+check_marks.grid(column=1, row=3)
+
+window.mainloop()
